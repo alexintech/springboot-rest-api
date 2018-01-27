90a44960b836003894cf651548010a2fe1b96a75
Alexander Cherepanov <xandr.cherepanov@gmail.com>
2018-01-27 19:31:37 +0600

    Basic rest controller on spring boot

---
+++ src/main/java/com/lsnotes/sandbox/springboot/restapi/AppEntry.java
@@ -0,0 +1,11 @@
+package com.lsnotes.sandbox.springboot.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppEntry {
+	public static void main(String[] args) {
+		SpringApplication.run(AppEntry.class, args);
+	}
+}
